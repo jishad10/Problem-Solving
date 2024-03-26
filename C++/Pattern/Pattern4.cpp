271c1c2431bcebc2243af84dee1b3bade904fc8c
Jahidul Islam Shikdar <118066090+jishad10@users.noreply.github.com>
2024-03-26 23:10:46 +0600

    solving pattern

---
+++ C++/Pattern/Pattern4.cpp
@@ -0,0 +1,29 @@
+/*
+output:
+       1 2 3
+       4 5 6
+       7 8 9
+*/
+//basic thinking hocce just count ko re
+//count er value golo print korteche
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    int i=1;
+    int count =1;
+    while(i<=n){
+        int j=1;
+        while(j<=n){
+            cout<<count<<" ";
+            count =count+1;
+            j=j+1;
+        }
+        cout<<endl;
+        i=i+1;
+    }
+}
