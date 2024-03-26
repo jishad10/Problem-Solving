86105d7a2e2db74eea25eb2aaf68bf2b3065ab56
Jahidul Islam Shikdar <118066090+jishad10@users.noreply.github.com>
2024-03-27 00:11:44 +0600

    solving pattern

---
+++ C++/Pattern/Pattern11.cpp
@@ -0,0 +1,30 @@
+/*
+output:
+      A B C D
+      E F G H
+      I J K L
+      M N O P
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    int row=1;
+    char count ='A';
+    while(row<=n){
+
+        int col=1;
+        while(col<=n){
+            cout<<count<<" ";
+            count =count+1;
+            col=col+1;
+        }
+        cout<<endl;
+        row=row+1;
+    }
+}
+
