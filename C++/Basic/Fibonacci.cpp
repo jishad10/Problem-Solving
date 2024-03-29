02376730772217130c1e5c2c326086a00528dca6
Jahidul Islam Shikdar <118066090+jishad10@users.noreply.github.com>
2024-03-30 02:41:03 +0600

    basic things

---
+++ C++/Basic/Fibonacci.cpp
@@ -0,0 +1,27 @@
+/*
+fibonacci series 0,1,1,2,3,5,8,13,21,34,55,89
+1st 2 ta add korle er porer ta ase
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cout<<"Enter a number : ";
+    cin>>n;
+
+    int a=0;
+    int b=1;
+    cout<<a<<" "<<b<<" ";
+    for(int i=1; i<=n; i++){
+
+        int nextNumber =a+b;
+        cout<<nextNumber<<" ";
+
+        //exchange
+        a=b;
+        b=nextNumber;
+    }
+}
---
+++ C++/Basic/PrimeNumber.cpp
@@ -0,0 +1,31 @@
+/*
+jodi kono value ke 1 theke soro ko re sey value er aag projonto
+kono sokka di e bag jai/rem aase taile prime noy.......
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cout<<"Enter a number : ";
+    cin>>n;
+
+    bool isPrime =1; //1st e do re nichi prime
+
+    for(int i=2; i<n; i++){
+
+        if(n%i ==0){
+            isPrime =0; //rem asle not prime
+            break;
+        }
+    }
+    if(isPrime == 0){
+        cout<<"not a prime number"<<endl;
+    }
+    else{
+        cout<<"is a prime number"<<endl;
+    }
+}
+
