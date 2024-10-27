67e01093dad5b529be5b996a0e74b8f6086be8bd
Jahidul Islam Shikdar <118066090+jishad10@users.noreply.github.com>
2024-10-28 02:07:48 +0600

    Find Unique In Array

--- C++/CodeStudio/FindUnique.cpp
+++ C++/CodeStudio/FindUnique.cpp
@@ -9,25 +9,22 @@ mani 1^2^3^4...^1 so
 #include<iostream>
 using namespace std;
 
-int findUnique(int arr[], int size){
+int findUnique(int *arr, int size)
+{
+    int ans =0;
 
-    int ans = 0;
-
-    for(int i =0; i<size; i++)
-    {
-        ans = ans ^ arr[i];
+    for(int i=0; i<size; i++){
+        ans=ans^arr[i];
     }
     return ans;
 }
 
 int main(){
 
-    int arr[9] = {1, 2, 3, 4, 5, 4, 3, 2, 1};
+    int arr[5] ={1,2,1,2,5};
 
-    int size;
-    int unique = findUnique(arr, 9);
-
-    cout<<"The unique number is : "<<unique;
+    int uniqueElement = findUnique(arr, 5);
+    cout << "The unique element is: " << uniqueElement << endl;
 
     return 0;
 }
