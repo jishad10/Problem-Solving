bdff183072d165a0548c259e840f9e5f1128ee73
Jahidul Islam Shikdar <118066090+jishad10@users.noreply.github.com>
2024-12-27 22:00:31 +0600

    First and Last Position of an Element In Sorted Array

---
+++ C++/CodeStudio/1st & Last Pos.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+
+int firstOcc(int arr[], int n, int key) {
+
+    int s = 0, e = n-1;
+    int mid = s + (e-s)/2;
+    int ans = -1;
+    while(s<=e)
+    {
+        if(arr[mid] == key){
+            ans = mid;
+            e = mid - 1;
+        }
+        else if(key > arr[mid]) {
+            s = mid + 1;
+        }
+        else{
+            e = mid - 1;
+        }
+        mid = s + (e-s)/2;
+    }
+    return ans;
+}
+
+int lastOcc(int arr[], int n, int key) {
+
+    int s = 0, e = n-1;
+    int mid = s + (e-s)/2;
+    int ans = -1;
+    while(s<=e)
+    {
+        if(arr[mid] == key){
+            ans = mid;
+            s = mid + 1;
+        }
+        else if(key > arr[mid]) {
+            s = mid + 1;
+        }
+        else{
+            e = mid - 1;
+        }
+        mid = s + (e-s)/2;
+    }
+    return ans;
+}
+
+int main(){
+
+    int even[12] = {1,2,2,3,3,3,3,3,3,3,4,5};
+
+    cout<<"First Occurrence of Index : "<<firstOcc(even,12,3)<<endl;
+    cout<<"Last Occurrence of Index : "<<lastOcc(even,12,3);
+
+    return 0;
+}
+
