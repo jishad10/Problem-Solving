d089721c0c665e50a2125f21a6340b7272df5774
Jahidul Islam Shikdar <118066090+jishad10@users.noreply.github.com>
2025-01-09 19:16:24 +0600

    Dynamic Memory Allocation

--- C++/Basic/Dynamic Allocation/2D_Array.cpp
+++ C++/Basic/Dynamic Allocation/2D_Array.cpp
@@ -8,13 +8,14 @@ int main() {
 
     int col;
     cin >> col;
-
+    
     //creating a 2D array
     int** arr = new int*[row];
     for(int i=0; i<row; i++) {
         arr[i] = new int[col];
     }
-
+    //row er jonno block create and every block er under e col create
+    
     //taking input
     for(int i=0; i<row; i++) {
         for(int j=0; j<col; j++) {
