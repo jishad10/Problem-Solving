6e0dbf22c7824751e074e2eedc1fbf1c33648e84
Jahidul Islam Shikdar <118066090+jishad10@users.noreply.github.com>
2025-03-06 23:15:45 +0600

    Stack

---
+++ C++/DSA/Stack/StackUsingSTL.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+
+int main(){
+
+  stack<int> s;
+
+  s.push(2);
+  s.push(3);
+
+  s.pop();
+
+  cout<<"Stack top "<< s.top() <<endl;
+
+  if(s.empty()){
+    cout<<"Stack is empty" <<endl;
+  }
+  else{
+    cout<<"Stack is not empty" <<endl;
+  }
+
+  return 0;
+}
