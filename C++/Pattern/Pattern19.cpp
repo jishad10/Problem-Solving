c6974753bd59daba1c280e60ad92d4515afd9b61
Jahidul Islam Shikdar <118066090+jishad10@users.noreply.github.com>
2026-04-07 21:30:30 +0600

    solving pattern

---
+++ C++/Pattern/Pattern19.cpp
@@ -0,0 +1,47 @@
+/*
+output:
+           1
+          121
+         12321
+        1234321
+       123454321
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin>>n;
+
+    int row =1;
+    while(row<=n){
+
+        //space print
+        int space=n-row;
+        while(space){
+            cout<<" ";
+            space=space-1;
+        }
+
+        //1st triangle print
+        int col =1;
+        while(col<=row) {
+            cout <<col;
+            col=col+1;
+        }
+
+        //2nd triangle print
+        int start=row-1;
+        while(start){
+            cout<<start;
+            start=start-1;
+        }
+
+        cout<<endl;
+        row=row+1;
+    }
+}
+
+
