185b2d04e92d0c15f4310e0216f4e6186fcfd5b7
Jahidul Islam Shikdar <118066090+jishad10@users.noreply.github.com>
2026-04-12 18:39:10 +0600

    Enhance comments in isPowerOfThree function
    Added comments to clarify the logic for checking if a number is a power of three.

--- C++/LeetCode/1st 10 Leetcode Problems/P-326.cpp
+++ C++/LeetCode/1st 10 Leetcode Problems/P-326.cpp
@@ -6,6 +6,9 @@ public:
     bool isPowerOfThree(int n){
         while(n>=3){
 
+            // if at ANY point n is NOT divisible by 3
+            // it can never be a power of 3 → return false"
+            // in that case if n%3 == 0 that means it's not 1 that's why it return true and loop continue
             if(n%3!=0)
             {
                 return false;
