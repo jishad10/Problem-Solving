61817dd7544ee96313184bdaea0dfe66f28102f9
Jahidul Islam Shikdar <118066090+jishad10@users.noreply.github.com>
2026-04-12 19:16:55 +0600

    Power of Four
    Refactor power of 2 check and add power of 4 condition

--- C++/LeetCode/1st 10 Leetcode Problems/P-342.cpp
+++ C++/LeetCode/1st 10 Leetcode Problems/P-342.cpp
@@ -11,11 +11,16 @@ public:
         }
 
         // Check if number is a power of 2
+        // jodi ami (num & (num - 1)) == 0;  return true; bole di, taile toh power 2 it's ok but tokonie ami true pai jacchi ar er porer line golo check korte ce nah
+        // amar toh 21 no line theke condition aace oi golo o toh check korte hobe tai ektu ulta trick kelchi
+        // mani condition ta tokonie false return korbe jokon 0 na hoi, 
+        // jodi 0 hoi that means true er porer line golo/condition e jabe, jodi 2 no line er comment er moto ko re ditam taile er porer line e jeto nah
         if((num & (num - 1))!= 0)
         {
             return false;
         }
 
+        // power of 4 means (num-1) % 3 == 0
         if((num - 1) % 3!=0)
         {
             return false;
